a9de3b4c560a6662201f966f2e2b48c410393c1a
telefonosuci <telefonosuci@gmail.com>
2016-12-18 23:05:04 +0100

    Added sails options

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,20 +3,24 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
 
   //namespace: 'api',
-  host: 'http://localhost:3000',
-  /*
+
+	// Port 3000 work with Rails as a back-end
+	//host: 'http://localhost:3000',
+
+	// Port 1337 work with Sails as a back-end
+	host: 'http://localhost:1337',
+	primaryKey: '_id'
+	/*
   buildURL: function(modelName, id, snapshot, requestType, query) {
         var url = this._super(modelName, id, snapshot, requestType, query);
         return url + ".json";
-  }
+  },
   */
-/*
-  ,
+	/*
   headers: {
     "Access-Control-Allow-Origin": "*"
-  }
-/*
-  ,
+  },
+	/*
   createRecord: function(store, type, snapshot){
     var promise = this._super(store, type, snapshot);
     console.log("Saving an item via POST");
